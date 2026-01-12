2742b519515c6271ad8c67cc1aa065da8de354c5
santosqapedro-droid <santosqapedro@gmail.com>
2026-01-12 17:13:10 -0300

    Create seed_data.sql
    Develop an SQL (INSERT) script to manually register the initial GxP systems in the data base, according to the ARI-5 history in Jira.

---
+++ seed_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO Software_Inventory (system_name, system_vendor, system_version, validation_date, validation_status, gamp_risk, created_by)
+VALUES
+('SAP S/4HANA', 'SAP', '2025.01', '', 'In validation', '5', 'Pedro A'),
+('Particle Measuring Systems', 'PMS', 'v8.8', '01/01/2026', 'Validated', '4', 'Pedro A'),
+('TrackWise', 'Sparta Systems', 'v10.6', '05/01/2026', 'Validated', '5', 'Pedro A'),
+('Veeva Vault QMS', 'Veeva', 'v23R2', '10/01/2026', 'Validated', '5', 'Pedro A'),
+('Empower 3', 'Waters', 'FR5', '12/01/2026', 'Validated', '4', 'Pedro');
